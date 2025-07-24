1b56e06072c22762c2b32165828149b992c76b7e
Lalit Jaiswal <83780467+imlkj@users.noreply.github.com>
2025-07-25 02:44:13 +0545

    Refactors gradient widget into a reusable StatelessWidget
    Extracts gradient container logic into a new `GradientContainer`
    class for improved reusability and code readability. Simplifies
    the `main` function and enhances maintainability by separating
    UI concerns into modular components.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
 
-//demo flutter 
+//demo flutter
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 40, 207, 129),
-                Color.fromARGB(219, 5, 48, 20),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(color: Colors.amberAccent, fontSize: 28),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
 
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 40, 207, 129),
+            Color.fromARGB(219, 5, 48, 20),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(color: Colors.amberAccent, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
